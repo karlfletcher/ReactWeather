e906be49b2b6ab6a1377308c58bd495ee585d51d
Karl Fletcher <karl.fletcher@q2q-it.com>
2017-04-19 09:22:40 +0100

    Added environment port to app listener

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var app = express();
+var PORT = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
-app.listen(3000, function(){
+app.listen(PORT, function(){
   console.log("Server running...");
 })
